5e983713c762646ea01cc7e742fcb563a3f7179e
sepehr9877 <sepehr.seifpour@yahoo.com>
2022-10-08 14:58:57 -0400

    Delete and Insert

--- Photo.py
+++ Photo.py
@@ -14,24 +14,32 @@ class Photo(Post):
         self.url=url
         self.thumbnailUrl=thumurl
         photo_object={"url":self.url,"title":self.photodes,"thumbnailUrl":self.thumbnailUrl}
-        self.Photo_object.append(photo_object)
         return photo_object
     def ReadApi(self):
         Photo_response = requests.get("https://jsonplaceholder.typicode.com/photos")
         Photo_data = json.loads(Photo_response.text)
-        for i in range(0, 2):
+        for i in range(0, 3):
             id=i+1
             url_item=Photo_data[i].get("url")
             title_item=Photo_data[i].get("title")
             thumbnailUrl_item=Photo_data[i].get("thumbnailUrl")
             Photo_elements={"id":id,"url":url_item,"title":title_item,"thumbnailUrl":thumbnailUrl_item}
             self.Photo_object.append(Photo_elements)
-        return Photo_elements
-    def Insert_Element(self,object,id):
-        self.Photo_object.append(object)
+    def Insert_Element(self,object):
+        self.setDescription(object["title"])
+        self.photodes=self.title
+        self.url=object["url"]
+        self.thumbnailUrl=object["thumbnailUrl"]
+        photo_item={"id":len(self.Photo_object)+1,"title":self.photodes,"thumbnailUrl":self.thumbnailUrl}
+        self.Photo_object.append(photo_item)
+        print(self.Photo_object)
         print("Run Insert Query For Photo")
     def Delete_Element(self,**kwargs):
-
+        id = kwargs["id"]
+        for index, item in enumerate(self.Photo_object):
+            if item.get("id") == id:
+                del self.Photo_object[index]
+        print(self.Photo_object)
         print("Delete Query for Photo")
     def Update_Element(self):
         print("Update Query for Photo")
--- main.py
+++ main.py
@@ -9,17 +9,23 @@ myelement=MakePost()
 photo=Photo()
 comment=Comment()
 photo.ReadApi()
-comment.ReadApi()
-comment_body="This is new Comment"
-comment_name="Sepehr"
-comment_email="Sepehr@yhaoo.com"
-comment2=comment(comment_body,comment_name,comment_email)
-comment3=comment(comment_body,comment_name,comment_email)
-myelement.MakeELement(comment)
-myelement.CallInsert(comment2)
-myelement.CallInsert(comment3)
-myelement.MakePost(comment.Comment_objects,photo.Photo_object)
-myelement.CallDelte(id=1)
+# comment.ReadApi()
+# comment_body="This is new Comment"
+# comment_name="Sepehr"
+# comment_email="Sepehr@yhaoo.com"
+# comment2=comment(comment_body,comment_name,comment_email)
+# comment3=comment(comment_body,comment_name,comment_email)
+# myelement.MakeELement(comment)
+# myelement.CallInsert(comment2)
+# myelement.CallInsert(comment3)
+# myelement.MakePost(comment.Comment_objects,photo.Photo_object)
+# myelement.CallDelte(id=1)
+photo2=photo(desc="THis is My new Photo",url="this is the  url",thumurl="thumurl")
+photo3=photo(desc="THis is My new Photo",url="this is the  url",thumurl="thumurl")
+myelement.MakeELement(photo)
+myelement.CallInsert(photo2)
+myelement.CallInsert(photo3)
+myelement.CallDelte(id=4)
 # comment(body="I am Sepehr",name="Sepehr",email="Sepehr@yahoo.com")
 # print(comment.Comment_objects)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
